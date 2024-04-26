a800ade36ce0ff9c9a4651f16eb6a43d2d5fe851
Nandana S <122261926+NandanaShankaramangalam@users.noreply.github.com>
2024-04-26 14:34:40 +0530

    Swagger documentation

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const calculatePrice = require("./controllers/pricingController");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
+
+const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.post("/calculate-price", calculatePrice);
+
+app.get("/", (req, res) => {
+  res.redirect("/api-docs");
+});
 
 app.listen(3000, () => {
   console.log("Server is listening at \nhttp://localhost:3000");
